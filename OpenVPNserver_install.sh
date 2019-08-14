be3f40943e2b20ede69695ba68a1decf08d6b6c6
Stephan Thierry <stephan@protek.dk>
2019-08-14 10:59:19 +0200

    Update OpenVPNserver_install.sh

--- OpenVPNserver_install.sh
+++ OpenVPNserver_install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 dig TXT +short o-o.myaddr.l.google.com @ns1.google.com
-sudo wget https://git.io/vpn -O openvpn-install.sh
-sudo bash openvpn-install.sh
+wget https://git.io/vpn -O openvpn-install.sh
+bash openvpn-install.sh
