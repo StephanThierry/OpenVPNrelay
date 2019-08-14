0966d26a780d337adc9bdaf32283f44c1435b122
Stephan Thierry <stephan@protek.dk>
2019-08-14 10:58:58 +0200

    Update IpTableRules.sh

--- IpTableRules.sh
+++ IpTableRules.sh
@@ -1,9 +1,10 @@
-sudo iptables -P INPUT ACCEPT
-sudo iptables -P FORWARD ACCEPT
-sudo iptables -P OUTPUT ACCEPT
-sudo iptables -t nat -F
-sudo iptables -t mangle -F
-sudo iptables -F
-sudo iptables -X
-sudo iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
-sudo apt-get install iptables-persistent
+#!/bin/bash
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -t nat -F
+iptables -t mangle -F
+iptables -F
+iptables -X
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+apt-get install iptables-persistent
